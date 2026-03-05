de9a806183bc4b78af1dc395e8fc805fe378c793
Tharindu Pathum <ptharindupathum2002@gmail.com>
2026-03-05 10:57:51 +0530

    First Commit

---
+++ src/main/java/lk/ijse/javafx/craftlankaproject/CraftLankaProjectApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.javafx.craftlankaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftLankaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CraftLankaProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/javafx/craftlankaproject/CraftLankaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.javafx.craftlankaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftLankaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
